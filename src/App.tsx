a214da6b89ea20ecd8ebedd0c5d1dafd6c559f99
Dongeun Kim <puro@protonmail.ch>
2024-09-23 14:55:43 +0900

    fix : png type fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import mrEggSushi from '@assets/icons/mr-egg-sushi-trans.png';
-import './styles/App.css';
+import "./styles/App.css";
 
 function App() {
   return (
@@ -7,7 +6,11 @@ function App() {
       <div className="w-dvw h-dvh flex flex-col justify-center items-center">
         <div className="w-40 h-40 animate-spin">
           <a href="https://github.com/kimpuro" target="_blank">
-            <img src={mrEggSushi} className="mr-egg-sushi" alt="mr-egg-sushi" />
+            <img
+              src="src/assets/icons/mr-egg-sushi-trans.png"
+              className="mr-egg-sushi"
+              alt="mr-egg-sushi"
+            />
           </a>
         </div>
         <h1 className="mb-2">Vite</h1>
